553a3f5b2ae6a1e915f308e5678c17878a2fcc9b
hia0706 <ljjs1215@gmail.com>
2024-04-29 12:44:12 +0900

    Feat 무작위 숫자 뽑기

--- 5강 숫자야구게임/number-baseball.js
+++ 5강 숫자야구게임/number-baseball.js
@@ -2,3 +2,15 @@ const $input = document.querySelector("#input");
 const $form = document.querySelector("#form");
 const $logs = document.querySelector("logs");
 
+const numbers = []; // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for (let n = 0; n <= 9; n++) {
+  numbers.push(n + 1);
+}
+
+const answer = [];
+for (let n = 0; n < 4; n++) { // 네 번 반복
+  const index = Math.floor(Math.random() * (numbers.length));
+  answer.push(numbers[index]);
+  numbers.splice(index, 1);
+}
+console.log(answer);
