3a70c15e88b1068253c8a5a7fea4c2e78f905e84
hia0706 <ljjs1215@gmail.com>
2024-04-29 12:56:49 +0900

    Feat input 입력값 검증

--- 5강 숫자야구게임/number-baseball.js
+++ 5강 숫자야구게임/number-baseball.js
@@ -14,3 +14,29 @@ for (let n = 0; n < 4; n++) { // 네 번 반복
   numbers.splice(index, 1);
 }
 console.log(answer);
+
+const tries = [];
+
+function checkInput(input) { // 검사하는 코드
+  if (input.length !== 4) { // 길이가 4가 맞는지?
+    return alert('4자리 숫자를 입력해주세요.');
+  }
+  if (new Set(input).size !== 4) { // 중복 값이 있는지?
+    return alert('중복되지 않게 입력해주세요.');
+  }
+  if (tries.includes(input)) { // 이미 시도한 값은 아닌지?
+    return alert('이미 시도한 값입니다.');
+  }
+  return true;
+}
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = $input.value;
+  $input.value = '';
+  if (checkInput(value)) { // 문제 없음
+    tries.push(value);
+  } else { // 에러 발생
+
+  }
+});
