9450c1d976360b2bdf09eb5e356ff3a8214f1878
hia0706 <ljjs1215@gmail.com>
2024-04-30 16:57:45 +0900

    Init 로또 추첨기 프로젝트 생성

---
+++ 6강 로또 추첨기/lotto.js
@@ -0,0 +1,11 @@
+const $form = document.querySelector("#form");
+const $result = document.querySelector("#result");
+const $bonus = document.querySelector("#bonus");
+
+function handdleSubmit(e) {
+  e.preventDefault();
+  // 검사
+
+}
+
+$form.addEventListener("submit", handdleSubmit);
